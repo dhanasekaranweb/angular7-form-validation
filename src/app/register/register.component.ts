517150a9f869132c79f4747075e464e11c3e6b6d
System Administrator <root@Dhanasekarans-MacBook-Air.local>
2019-03-16 23:51:33 +0530

    register form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,17 +9,21 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 export class RegisterComponent implements OnInit {
 
 	registerForm:FormGroup
+	submitted:boolean
 
   constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit() {
+  	this.submitted = false
   	this.registerForm = this.formBuilder.group({
   		firstname:['',Validators.required],
   		lastname:['',Validators.required],
   		username:['',[Validators.required,Validators.minLength(6)]],
-  		email:['',Validators.required],
-  		phonenumber:['',Validators.required],
-  		password:['',Validators.required]
+  		email:['',[Validators.required,Validators.email]],
+  		password:['',[Validators.required,Validators.minLength(8)]],
+  		cpassword:['',Validators.required]
+  	},{
+  		validator:this.passmatch('password','cpassword')
   	})
   }
 
@@ -27,7 +31,27 @@ export class RegisterComponent implements OnInit {
   	return this.registerForm.controls;
   }
 
+  passmatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        let control = formGroup.controls[controlName];
+        let matchingControl = formGroup.controls[matchingControlName];
+
+        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+            return;
+        }
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ mustMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
+}
+
   onRegister(){
+  	this.submitted = true
+  	if(this.registerForm.invalid){
+  		return;
+  	}
   	console.log(this.registerForm.controls)
   }
 
